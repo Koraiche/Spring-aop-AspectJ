46a8255d123ce7f8ffc9c65bb203a1c0303bb0fc
fkoraiche <fkoraiche@s2m.ma>
2023-01-10 11:09:28 +0100

    init

--- ship/package/mobile-switch-settings/config/update-oracle.sql
+++
@@ -1,8 +0,0 @@
-
-alter session set current_schema = NXP_SMNM_SETTINGS;
-
-ALTER TABLE center drop column end_of_day_time;
-
-ALTER TABLE institution drop column debit_endpoint;
-
-ALTER TABLE institution add column check_endpoint;
